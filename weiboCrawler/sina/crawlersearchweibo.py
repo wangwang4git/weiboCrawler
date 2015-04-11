1ac62f7fd2b5241dab0058468e9383bd0e9950d9
Wang Wang <wangwang4whu@126.com>
2015-04-12 00:06:52 +0800

    1.Add a file.

---
+++ weiboCrawler/sina/crawlersearchweibo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-04-11 23:48:33
+# @Author  : Wwang (wangwang4whu@126.com)
+# @Link    : https://github.com/wangwang4git
+# @Version : $Id$
+
+# http://s.weibo.com/wb/haha&xsort=time&scope=ori&timescope=custom:2015-04-11-0:2015-04-11-12&page=2
+# http://s.weibo.com/wb/【关键词】&xsort=time&scope=ori&timescope=custom:【起始时间】:【终止时间】&page=【页码】
+import os
